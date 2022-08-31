07fa41131ad7e230a5506e670fc41dbac581473c
VuongDB <dobavuong_t62@hus.edu.vn>
2022-08-31 15:49:27 +0700

    update pagination

--- Course/src/main/java/com/example/course/controller/UserController.java
+++ Course/src/main/java/com/example/course/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.course.controller;
 
+import com.example.course.entity.Course;
 import com.example.course.service.CourseService;
 import com.example.course.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -16,9 +20,12 @@ public class UserController {
     private TopicService topicService;
 
 
-    @GetMapping ("/")
-    public String getUserCourseList(Model model) {
-        model.addAttribute("courseList",courseService.getAllCourses());
+    @GetMapping ("/index")
+    public String getUserCourseList(Model model, Optional<Integer> currentPage) {
+        int pageNo = currentPage.orElse(0);
+        Page<Course> page = courseService.getAllCourses(pageNo);
+        System.out.println("test=================== "+ page.getTotalPages());
+        model.addAttribute("courseList",page);
         model.addAttribute("topicList",topicService.getAllTopics());
         return "template-course/course-list";
     }
--- Course/src/main/java/com/example/course/service/CourseService.java
+++ Course/src/main/java/com/example/course/service/CourseService.java
@@ -4,6 +4,9 @@ import com.example.course.entity.Course;
 import com.example.course.repository.CourseRepository;
 import com.example.course.repository.TopicRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +17,8 @@ public class CourseService {
     @Autowired
     private CourseRepository courseRepository;
 
-    public List<Course> getAllCourses() {
-        return courseRepository.findAll();
+    public Page<Course> getAllCourses(int pageNo) {
+        Pageable pageable = PageRequest.of(pageNo, 9);
+        return courseRepository.findAll(pageable);
     }
 }
